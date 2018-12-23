48a3e29b90519b9d154e2d132738d0a741393702
choonkeat <choonkeat@gmail.com>
2018-12-23 14:05:05 +0800

    extract bash functions

--- tests/scenario.sh
+++ tests/scenario.sh
@@ -2,42 +2,12 @@
 
 # abort on any failure
 set -e
-
+source `dirname $0`/lib.sh
 
 # setup
 DBMIGRATE_CMD='go run main.go'
 DB_MIGRATIONS_DIR=tests/db/migrations
-if test -n "`bash -version | grep 'version 3.2.'`"; then
-    ESC="\x1B"
-else
-    ESC="\e"
-fi
-function assert() {
-    desc=$1
-    shift
-    if $* 2>&1 > tests/output.log; then
-        echo -e "${ESC}[42m${ESC}[97m[PASS]${ESC}[39m${ESC}[0m ${desc}"
-        rm -f tests/output.log
-    else
-        echo -e "${ESC}[41m${ESC}[97m[FAIL]${ESC}[39m${ESC}[0m ${desc}"
-        cat tests/output.log
-        exit 1
-    fi
-}
-function assert_fail() {
-    desc=$1
-    shift
-    if $*; then
-        echo -e "${ESC}[41m${ESC}[97m[FAIL]${ESC}[39m${ESC}[0m ${desc}"
-        exit 1
-    else
-        echo -e "${ESC}[42m${ESC}[97m[PASS]${ESC}[39m${ESC}[0m ${desc}"
-    fi
-}
 
-function finish() {
-  rm -rf ${DB_MIGRATIONS_DIR}
-}
 trap finish EXIT
 mkdir -p ${DB_MIGRATIONS_DIR}
 echo "testing ${DATABASE_DRIVER}..."
