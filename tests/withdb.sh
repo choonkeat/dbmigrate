207970b43ac2bc039d21b9bb54f7721f3bc505e6
choonkeat <choonkeat@gmail.com>
2018-12-27 09:37:25 +0800

    improve setup of mysql/mariadb test

--- tests/withdb.sh
+++ tests/withdb.sh
@@ -20,23 +20,21 @@ case $DATABASE_DRIVER in
     postgres)
     docker run --rm -e POSTGRES_DB=${DB_NAME} -e POSTGRES_PASSWORD=${DB_PASSWORD} -p ${PORT}:5432 -d --cidfile cid.txt postgres
     echo "waiting for ${DATABASE_DRIVER}...";
-    until docker exec -it `cat cid.txt` pg_isready 2>&1 > /dev/null; do sleep 3; done
+    until docker exec -it `cat cid.txt` pg_isready; do sleep 3; done
     env DATABASE_DRIVER=${DATABASE_DRIVER} DATABASE_URL="postgres://postgres:${DB_PASSWORD}@localhost:${PORT}/${DB_NAME}?sslmode=disable" DB_MIGRATIONS_DIR=${DB_MIGRATIONS_DIR} bash ${TARGET_SCRIPT}
     finish
     ;;
     mysql)
-    docker run --rm -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p ${PORT}:3306 -d --cidfile cid.txt mysql --default-authentication-plugin=mysql_native_password
+    docker run --rm -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -p ${PORT}:3306 -d --cidfile cid.txt mysql --default-authentication-plugin=mysql_native_password
     echo "waiting for ${DATABASE_DRIVER}...";
-    until docker exec -it `cat cid.txt` mysql -e 'SELECT 1' 2>&1 > /dev/null; do sleep 3; done
-    sleep 20 # ready not ready
+    until docker exec -it `cat cid.txt` mysql -u root -p${DB_PASSWORD} --protocol TCP -e 'SELECT 1'; do sleep 3; done
     env DATABASE_DRIVER=${DATABASE_DRIVER} DATABASE_URL="root:${DB_PASSWORD}@tcp(127.0.0.1:${PORT})/${DB_NAME}?multiStatements=true" DB_MIGRATIONS_DIR=${DB_MIGRATIONS_DIR} bash ${TARGET_SCRIPT}
     finish
     ;;
     mariadb)
     docker run --rm -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -p ${PORT}:3306 -d --cidfile cid.txt mariadb
     echo "waiting for ${DATABASE_DRIVER}...";
-    until docker exec -it `cat cid.txt` mysql -e 'SELECT 1' 2>&1 > /dev/null; do sleep 3; done
-    sleep 20 # ready not ready
+    until docker exec -it `cat cid.txt` mysql -u root -p${DB_PASSWORD} --protocol TCP -e 'SELECT 1'; do sleep 3; done
     env DATABASE_DRIVER=mysql DATABASE_URL="root:${DB_PASSWORD}@tcp(127.0.0.1:${PORT})/${DB_NAME}?multiStatements=true" DB_MIGRATIONS_DIR=${DB_MIGRATIONS_DIR} bash ${TARGET_SCRIPT}
     finish
     ;;
